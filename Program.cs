745ff62e877efce6581eb65e84c4bc5d9941e070
g10101k <g101k@mail.ru>
2019-02-16 14:18:04 +0300

    Мелкие правки

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+/*
+ *  "gk.MonitorAndCopy" - The utility for copying new files by SMB protocol 
+ *
+ *  Copyright (C) 2015-2019  Igor Tyulyakov aka g10101k, g101k. Contacts: <g101k@mail.ru>
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Threading;
@@ -34,16 +51,16 @@ namespace gk.MonitorAndCopy
                 }
                 if (!Directory.Exists(DestPath))
                 {
-                    MessageBox.Show(MonPath + " - Не существует DestPath! \r\n Выход из программы");
+                    MessageBox.Show(DestPath + " - Не существует DestPath! \r\n Выход из программы");
                     return;
                 }
                 if (!Directory.Exists(BakPath))
                 {
-                    MessageBox.Show(MonPath + " - Не существует BakPath! \r\n Выход из программы");
+                    MessageBox.Show(BakPath + " - Не существует BakPath! \r\n Выход из программы");
                     return;
                 }
 
-                while (1 == 1)
+                while (true)
                 {
                     string[] allFoundFiles = Directory.GetFiles(MonPath, mask, SearchOption.AllDirectories);
                     foreach (string f in allFoundFiles)
